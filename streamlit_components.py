892db84d8be61544b7922889d64f2be701a0103d
Andrea Melleu <andreamelleu@gmail.com>
2026-01-15 18:14:03 -0300

    Content Update: Added instruction to download report and upload to Drive

--- streamlit_components.py
+++ streamlit_components.py
@@ -15,6 +15,8 @@ MESSAGES = {
         **Olá, produtor Teatrali!**  
         Após criar as suas artes conforme o checklist, suba e veja os avisos do sistema, em vermelho delete. Depois valide o formato das suas artes, até finalizar todas elas.
         
+        **Baixe o relatório e suba tudo no drive da sua atração.**
+        
         Qualquer dúvida, fale com o Procópio e solicite o que precisar!
     """,
     "selecione_teatro": "Selecione o Teatro:",
